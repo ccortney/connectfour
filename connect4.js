01436743ccaac8ce9806aba299a9810705cb4ca8
Camilla Cortney <camilla.cortney@gmail.com>
2021-09-25 22:29:10 -0700

    Second Upload

--- connect4.js
+++ connect4.js
@@ -10,6 +10,10 @@ const HEIGHT = 6; // y
 
 let currPlayer = 'player1'; // active player: 1 or 2
 const board = []; // array of rows, each row is array of cells  (board[y][x])
+const playerTitle = document.querySelector('#player-title');
+playerTitle.innerText = `${currPlayer}`;
+
+
 
 /** makeBoard: create in-JS board structure:
  *    board = array of 6 rows(height), each row is array of 7 cells(width)  (board[y][x])
@@ -17,14 +21,15 @@ const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 function makeBoard() {
   // TODO: set "board" to empty HEIGHT x WIDTH matrix array
-  // Creates an array with 7 null elements (the size of the width)
-  const rowArr = []
-  for (let x=0; x<WIDTH; x++) {
-    rowArr.push(null);
-  }
-  // Pushes each row array into the board array in order to have a height of 6
+
   for (let y=0; y<HEIGHT; y++) {
-    board.push(rowArr)
+    // Creates an array with 7 null elements (the size of the width)
+    const rowArr = [];
+    for (let x=0; x<WIDTH; x++) {
+      rowArr.push(null);
+    }
+    // Pushes each row array into the board array in order to have a height of 6
+    board.push(rowArr);
   }
 }
 
@@ -70,7 +75,8 @@ function makeHtmlBoard() {
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
+// Search the x olumn you clicked to find the lowest y cell that is not occupied. Return that y value. 
+// If the entire column is filled, return null.
   for (let y=HEIGHT-1; y>=0; y--) {
     if (!board[y][x]) {
       return y;
@@ -83,17 +89,23 @@ function findSpotForCol(x) {
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
-  // TODO: make a div and insert into correct table cell
+    // create a new div that will be the game piece
     const newPiece = document.createElement("div");
+    // give the div/piece the two classes
+    // the piece class makes it look like a game piece
+    // the currPlayer class changes the color (red/blue)
     newPiece.classList.add("piece", `${currPlayer}`);
+    // find the bottom cell using the id
     const cell = document.getElementById(`${y}-${x}`);
+    // add the game piece to the cell
     cell.append(newPiece);
-    }
+  }
     
 /** endGame: announce game end */
 
 function endGame(msg) {
-  // TODO: pop up alert message
+  // pop up alert message with the winner
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
@@ -104,13 +116,13 @@ function handleClick(evt) {
 
   // get next spot in column (if none, ignore click)
   const y = findSpotForCol(x);
-  if (y === null) {
+  if (y===null) {
     return;
   }
 
+  // add current player to the array to indicate that it is filled by a game piece
+  board[y][x] = currPlayer;
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-
   placeInTable(y, x);
 
   // check for win
@@ -119,12 +131,20 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-
-  // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  // check if all cells in board are filled; if so call, call endGame
+  let filledRow = 0;
+  for (let y=0; y<HEIGHT; y++) {
+    if (!board[y].includes(null)) {
+      filledRow++;
+    };
+    if (filledRow === HEIGHT) {
+    return endGame('It is a tie!');
+    }
+  }
+  
+  // change player
   currPlayer === 'player1' ? currPlayer = 'player2' : currPlayer = 'player1';
-
+  playerTitle.innerText = `${currPlayer}`;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -132,9 +152,8 @@ function handleClick(evt) {
 function checkForWin() {
   function _win(cells) {
     // Check four cells to see if they're all color of current player
-    //  - cells: list of four (y, x) cells
+    //  - cells: array of four (y, x) cells
     //  - returns true if all are legal coordinates & all match currPlayer
-
     return cells.every(
       ([y, x]) =>
         y >= 0 &&
@@ -146,14 +165,20 @@ function checkForWin() {
   }
 
   // TODO: read and understand this code. Add comments to help you.
+  // Loop through each row
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      // Determine if there is a horizontal win
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      // Determine if there is a vertical win
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      // Determine if there is a diagonal down right win
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      // Determine if there is a diagonal down left win
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
-
+      // Run the _win function with each type or win
+      // If one of the win types return true, that player wins!
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
       }
